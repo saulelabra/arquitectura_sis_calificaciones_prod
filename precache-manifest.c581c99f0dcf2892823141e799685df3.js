9bfd3e6fe52bbcaf0ad6ad40e98172219d07a830
saulelabra <quiquelabra@gmail.com>
2019-11-18 19:24:21 -0600

    first commit

---
+++ precache-manifest.c581c99f0dcf2892823141e799685df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cb6edd2fd8d5adf35c061d44cc0897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f97d511ff3b79d0963",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "691172822531962b9c9d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a4f97d511ff3b79d0963",
+    "url": "/static/js/2.8858946c.chunk.js"
+  },
+  {
+    "revision": "691172822531962b9c9d",
+    "url": "/static/js/main.fc31bddd.chunk.js"
+  },
+  {
+    "revision": "5825175ab827da4d1cff",
+    "url": "/static/js/runtime-main.6ea4bbd7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
